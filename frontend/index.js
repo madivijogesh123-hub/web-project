2b36fdec1c2db13af56552def2f39e710d54905a
madivijogesh123-hub <madivijogesh123@gmail.com>
2026-08-14 11:10:16 +0530

    images changed

--- frontend/index.js
+++ frontend/index.js
@@ -7,7 +7,7 @@ const users = [
  },
    {
    "name" : "Jane Doe",
-   "gender" : "Feale",
+   "gender" : "Female",
    "image" : "jane.png"
  }
 ]
@@ -25,5 +25,21 @@ function toggle(){
 
  document.getElementById("card-image").src = users[curIndex].image
  document.getElementById("card-name").innerText =users[curIndex].name
+ document.getElementById("card-gender").innerText =users[curIndex].gender
  
+}
+
+function random(){
+	fetch('https://randomuser.me/api')
+	.then(function(response){
+        return response.json();
+        })
+.then(function(data){
+var  detail = data.results[0];
+document.getElementById("card-image").src = detail.picture.large;
+document.getElementById("card-gender").innerText = detail.gender;
+var fullname = detail.name.first+"  "+detail.name.last
+document.getElementById("card-name").innerText = fullname;
+
+})
 }
